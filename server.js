9f4637943618c32e2d3d48542ab9a9c269eccab7
RBG74 <remy.bernardgranger@gmail.com>
2018-05-31 16:55:44 +0200

    Démare un server sur 5001 ou + si pas dispo

---
+++ server.js
@@ -0,0 +1,25 @@
+const net = require("net");
+
+const hostname = "127.0.0.1";
+let port = 5001;
+
+let serverStarted = false;
+
+createServer(hostname, port);
+
+function createServer(hostname, port) {
+    const server = net.createServer((req, res) => {
+        res.end(`Server ${port} reporting for duty.`);
+        console.log(`Server ${port} reporting for duty.`);
+    });
+
+    server.listen(port, hostname, () => {
+        console.log(`Server ${port} started.`);
+        serverStarted = true;
+    });
+    server.on("error", err => {
+        if (err.code == 'EADDRINUSE') {
+            createServer(hostname, port+1);
+        }
+    });
+}
